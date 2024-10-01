5c58f7ca466782000b58edd06b399835ced53527
AJ Jordan <alex@strugee.net>
2024-10-01 03:44:13 -0400

    Fix `podman push` syntax

--- build-and-push.sh
+++ build-and-push.sh
@@ -3,4 +3,6 @@
 set -euo pipefail
 
 ./build.sh $1
-podman push ubuntu-server-dev:$1 $(podman images --format='{{.Repository}}:{{.Tag}}' | grep localhost/ubuntu-server-dev | tr '\n' ' ' | sed 's;localhost/;ghcr.io/seagl/ubuntu-server-dev/;g')
+for i in $(podman images --format='{{.Repository}}:{{.Tag}}' | grep localhost/ubuntu-server-dev | tr '\n' ' ' | sed 's;localhost/;ghcr.io/seagl/ubuntu-server-dev/;g'); do
+	podman push ubuntu-server-dev:$1 $i
+done
