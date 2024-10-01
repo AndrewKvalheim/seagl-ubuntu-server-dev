5f5a6ab4189a1bf557a76b7707118660363a1151
AJ Jordan <alex@strugee.net>
2024-10-01 04:17:48 -0400

    Fix duplicated namespace key
    We were pushing to ghcr.io/seagl/ubuntu-server-dev/ubuntu-server-dev.

--- build-and-push.sh
+++ build-and-push.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 ./build.sh $1
-for i in $(podman images --format='{{.Repository}}:{{.Tag}}' | grep localhost/ubuntu-server-dev | tr '\n' ' ' | sed 's;localhost/;ghcr.io/seagl/ubuntu-server-dev/;g'); do
+for i in $(podman images --format='{{.Repository}}:{{.Tag}}' | grep localhost/ubuntu-server-dev | tr '\n' ' ' | sed 's;localhost/;ghcr.io/seagl/;g'); do
 	podman push ubuntu-server-dev:$1 $i
 done
