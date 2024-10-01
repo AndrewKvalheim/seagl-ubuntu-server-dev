f52a671dbb39c9927d154fc8ea03975ccc4a994e
AJ Jordan <alex@strugee.net>
2024-10-01 03:41:04 -0400

    `set -euo pipefail` in build-and-push

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+set -euo pipefail
 
 ./build.sh $1
 podman push ubuntu-server-dev:$1 $(podman images --format='{{.Repository}}:{{.Tag}}' | grep localhost/ubuntu-server-dev | tr '\n' ' ' | sed 's;localhost/;ghcr.io/seagl/ubuntu-server-dev/;g')
